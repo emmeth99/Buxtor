8b5afafe903016e42130836a670dd823ce5fc222
emmeth99 <fantaexotic099@gmail.com>
2020-11-10 10:34:14 +0100

    a

--- produkter.php
+++ produkter.php
@@ -17,10 +17,10 @@
 
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="produkter.html">Kategorier</a>
+                <a class="nav-link" href="produkter.php">Kategorier</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="produkter.html">Författare</a>
+                <a class="nav-link" href="produkter.php">Författare</a>
             </li>
             <li class="nav-item2">
                 <a class="nav-link" href="#">Kassa</a>
@@ -35,6 +35,22 @@
 
     <div class="bild">
         Produkter hääär!
+        <?php
+            $servername = "127.0.0.1";
+            $username = "991001";
+            $password = "blåfisken";
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password);
+
+            // Check connection
+            if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+            }
+            echo "Connected successfully";
+
+            $conn->close();
+        ?>
     </div>
 
 
