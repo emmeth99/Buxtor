9478ef4ab405f360bfb5866ad6c67e6766af4b3c
emmeth99 <fantaexotic099@gmail.com>
2020-11-17 12:35:04 +0100

    aa

--- minsida.php
+++ minsida.php
@@ -19,7 +19,6 @@
     $cookie_value = $id;
     setcookie($cookie_name, $cookie_value, time() + (86400), "/"); 
 
-    echo "value: ".$cookie_value;
 
     $user = $_COOKIE[$cookie_name];
 
@@ -30,19 +29,18 @@
     $rank = $row['Rank'];
 
     $_SESSION["rank"] = $rank;
-    echo "rank: " . $_SESSION["rank"];
 
     $conn->close();
 
     if(isset($_COOKIE["user"] && $_SESSION["rank"] == "admin"){
-        header("Location: http://http://92.32.39.21:8080/minsida_admin.php");
+        header("Location: http://92.32.39.21:8080/minsida_admin.php");
         exit;
     }elseif(isset($_COOKIE["user"] && $_SESSION["rank"] == "kund"){
-        header("Location: http://http://92.32.39.21:8080/minsida_kund.php");        
+        header("Location: http://92.32.39.21:8080/minsida_kund.php");        
         exit;
     }else{
-        header("Location: http://http://92.32.39.21:8080/login.php"); /* Redirect browser */
-        exit();
+        header("Location: http://92.32.39.21:8080/login.php"); /* Redirect browser */
+        exit;
     }   
 
 
