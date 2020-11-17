b2bbd951a8f932a06e4a9b1d3fc4831f8f3a24c6
emmeth99 <fantaexotic099@gmail.com>
2020-11-17 12:27:48 +0100

    aa

--- minsida.php
+++ minsida.php
@@ -34,80 +34,23 @@
 
     $conn->close();
 
+    if(isset($_COOKIE["user"] && $_SESSION["rank"] == "admin"){
+        header("Location: http://http://92.32.39.21:8080/minsida_admin.php");
+        exit;
+    }elseif(isset($_COOKIE["user"] && $_SESSION["rank"] == "kund"){
+        header("Location: http://http://92.32.39.21:8080/minsida_kund.php");        
+        exit;
+    }else{
+        header("Location: http://http://92.32.39.21:8080/login.php"); /* Redirect browser */
+        exit();
+    }   
+
+
 ?>
 
 <html>
-<head>
-<title>Buxtor</title>
-<link rel="stylesheet" href="css/login.css">
-<link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@700&display=swap" rel="stylesheet">
-</head>
-<body>
-    
-    <?php include "header.php" ?>
-    
 
-    <nav class="navbar">
 
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="produkter.php">Kategorier</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="produkter.php">Författare</a>
-            </li>
-            <li class="nav-item2">
-                <a class="nav-link" href="#">Kassa</a>
-            </li>
 
-            <?php if(isset($_COOKIE["user"] && $_SESSION["rank"] == "admin"){
-                    echo '<li class="nav-item2">
-                    <a class="nav-link" href="minsida_admin.php">Admin</a>
-                    </li>';
-                }else if(isset($_COOKIE["user"] && $_SESSION["rank"] == "kund"){
-                    echo '<li class="nav-item2">
-                    <a class="nav-link" href="minsida_kund.php">Min sida/a>
-                    </li>';
-                }else{
-                    echo '<li class="nav-item2">
-                    <a class="nav-link" href="login.php">Logga in</a>
-                    </li>';
-                }   
-            ?>
-        </ul>
-        
-        
-    </nav>
-    <div class="container">
 
-        <div class="bild">
-
-            <?php
-                
-
-                if($rank == "admin"){
-                    include "minsida_admin.php";
-                }else if($rank == "kund"){
-                    include "minsida_kund.php";
-                }
-                
-                
-
-                if(isset($_COOKIE[$cookie_name])) {
-                    echo "Cookie named '" . $cookie_name . "' is set!";
-                    echo "Value is: " . $_COOKIE[$cookie_name];
-                } else {
-                    echo "Cookie '" . $cookie_name . "' is not set!<br>";
-                    
-                }
-
-                
-
-            ?>
-
-        </div>
-            
-    </div>
-
-</body>
 </html>
