6907fe97e4f1e24d888cb60cba2b8799d4085b06
Simethi <elviis15@hotmail.com>
2020-11-18 16:15:07 +0100

    hej

---
+++ action_skapauser.php
@@ -0,0 +1,28 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<meta charset="UTF-8">
+
+
+<?php
+
+
+include "connectDB.php";
+
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$email = $_POST['email'];
+$pwd = $_POST['pwd'];
+
+
+$sql = "INSERT INTO `Konto`(`Förnamn`, `Efternamn`, `Email`, `Rank`, `Lösenord`) 
+VALUES ($fname,$lname,$email,"kund",$pwd)";
+
+if ($conn->query($sql) == TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
