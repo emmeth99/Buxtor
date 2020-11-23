a2e5f53b59f879baaa281cd462267fa41571ca6b
emmeth99 <fantaexotic099@gmail.com>
2020-11-23 15:03:00 +0100

    aa

--- info_produkt.php
+++ info_produkt.php
@@ -13,6 +13,19 @@
 
 </head>
 <body>
+
+
+
+<?php 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    include "connectDB.php";
+    $sql = "INSERT INTO `Varukorg`(`Förnamn`, `Efternamn`, `Email`, `Rank`, `Lösenord`) 
+    VALUES ('$fname','$lname','$email','kund','$pwd')";
+}
+
+
+?>
     
     <?php include "header.php" ?>
     
@@ -49,6 +62,13 @@
         <div class="bild">
             <img src="<?php echo $row["Bild"] ?>" class="produktBild">
             <h2> <?php echo $row['Pris'] ?> kr </h2>
+            
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                <label for="pris">Antal:</label>
+                <input type="number" id="antal" name="antal" value= "1">
+                <input type="submit" value="KÖP"><br><br>
+            </form>
+
         </div>
         
 
