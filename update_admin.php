d3b14a20bf1b0262cc48cf44cc212fa50a682424
Simethi <elviis15@hotmail.com>
2020-11-23 14:32:53 +0100

    hej

--- update_admin.php
+++ update_admin.php
@@ -7,6 +7,18 @@
 <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@700&display=swap" rel="stylesheet">
 </head>
 <body>
+
+<?php 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $article = test_input($_POST["article"]);
+    }
+
+if(isset($article)){
+    $sql = "SELECT * FROM Vara Where 'ArtkelNr' = '$article'";
+            $result = $conn->query($sql);
+            $row = $result->fetch_assoc();
+}
+?>
     
     <?php include "header.php" ?>
     
@@ -19,17 +31,21 @@
         <div class="skapa">
             <h2>Redigera vara</h2>
 
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                <label for="article">Artikelnr:</label><br>
+                <input type="number" id="article" name="article" required><br>
+                <input type="submit" value="Hämta"><br><br>
+            </form>
+
             <form action="action_update_admin.php" method="post">
-            	<label for="article">Artikelnr:</label><br>
-                <input type="number" id="article" name="article" required><br><br><br>
                 <label for="name">Artikelnamn:</label><br>
-                <input type="text" id="name" name="name"><br>
+                <input type="text" id="name" name="name" value = <?php echo $row["ArtikelNamn"] ?>><br>
                 <label for="pris">Pris:</label><br>
-                <input type="number" id="pris" name="pris"><br>
+                <input type="number" id="pris" name="pris" value= <?php echo $row["Pris"] ?>><br>
                 <label for="author">Författare:</label><br>
-                <input type="text" id="author" name="author"><br>
+                <input type="text" id="author" name="author" value= <?php echo $row["Författare"] ?>><br>
                 <label for="besk">Beskrivning:</label><br>
-                <input type="text" id="besk" name="besk" ><br><br>
+                <input type="text" id="besk" name="besk" value= <?php echo $row["Beskrivning"] ?>><br><br>
   
                 <label for="genre">Genre:</label>
                 <select id="genre" name="genre">
