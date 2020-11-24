76f84761889b95a83dfc3cb19d433d5164472840
Simethi <elviis15@hotmail.com>
2020-11-24 11:44:00 +0100

    hej

--- action_delete_admin.php
+++ action_delete_admin.php
@@ -14,6 +14,12 @@ $article = $_POST['article'];
 
 $sql = "DELETE FROM Vara Where ArtikelNr = $article";
 
+if ($conn->query($sql) == TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
 $conn->close();
 
 header("Location: http://92.32.39.21:8080/minsida_admin.php");
