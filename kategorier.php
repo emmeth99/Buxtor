e87d7cb537006938c89fa133520c3f1ff59326f4
emmeth99 <fantaexotic099@gmail.com>
2020-12-01 11:53:58 +0100

    aa

--- kategorier.php
+++ kategorier.php
@@ -17,6 +17,17 @@
 
     <div class="container">
 
+
+    <div class="nav2">
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">About</a></li>
+        </ul>
+    
+    </div>
+
     <div class="bild">
     
 
